bdfb895cafb4a4e5c90e19ee4006bbaed1897d26
Desmond Furtick <desmond.furtick@helpcloud.local>
2019-11-19 18:23:20 -0700

    challenge complete

---
+++ name-challenge.ex
@@ -0,0 +1,10 @@
+name = IO.gets "What is your first name"
+
+case String.trim(name) do
+"desmond" -> IO.puts("Eureka! That's the name of my creator")
+"Desmond" -> IO.puts("Eureka! That's the name of my creator")
+_ -> IO.puts(~s{You're name is #{name}})
+end
+
+
+
