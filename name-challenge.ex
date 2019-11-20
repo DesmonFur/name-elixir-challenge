143cadc60b5db941da0d6c38fbd1116efc2d1ed6
Desmond Furtick <desmond.furtick@helpcloud.local>
2019-11-19 18:31:34 -0700

    wowzers

--- name-challenge.ex
+++ name-challenge.ex
@@ -1,9 +1,14 @@
-name = IO.gets "What is your first name"
+defmodule  do
+
+  def start do
+    name = IO.gets "What is your first name"
 
 case String.trim(name) do
 "desmond" -> IO.puts("Eureka! That's the name of my creator")
 "Desmond" -> IO.puts("Eureka! That's the name of my creator")
 _ -> IO.puts(~s{You're name is #{name}})
+end
+end
 end
 
 
